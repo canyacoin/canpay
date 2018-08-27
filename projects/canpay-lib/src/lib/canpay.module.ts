90c9be7f5f6dcf9995ab9fa230a9355bb20725ed
alsco77 <alsco77@pm.me>
2018-08-27 15:46:43 +1000

    Fixed prod build error and published new version

--- projects/canpay-lib/src/lib/canpay.module.ts
+++ projects/canpay-lib/src/lib/canpay.module.ts
@@ -1,25 +1,27 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { BootstrapModalModule } from 'ng2-bootstrap-modal';
+import { ModuleWithProviders } from '@angular/compiler/src/core';
+import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BootstrapModalModule } from 'ng2-bootstrap-modal';
 
+import { BancorWcComponent } from './bancor-wc/bancor-wc.component';
+import { CanpayModalComponent } from './canpay-modal/canpay-modal.component';
 import { CanpayWizardComponent } from './canpay-wizard/canpay-wizard.component';
-import { MetamaskComponent } from './metamask/metamask.component';
-import { EthService } from './services/eth.service';
+import { CommaSepNumPipe } from './comma-sep-num.pipe';
+import { InputAmountComponent } from './input-amount/input-amount.component';
+import { LoadingStatusComponent } from './loading-status/loading-status.component';
 import { FaqComponent } from './metamask/faq/faq.component';
 import { InstructionsComponent } from './metamask/instructions/instructions.component';
-import { LoadingStatusComponent } from './loading-status/loading-status.component';
-import { BancorWcComponent } from './bancor-wc/bancor-wc.component';
-import { CommaSepNumPipe } from './comma-sep-num.pipe';
-import { PaymentAuthorisationComponent } from './payment-authorisation/payment-authorisation.component';
-import { PaymentComponent } from './payment/payment.component';
+import { MetamaskComponent } from './metamask/metamask.component';
 import { MsgBoxComponent } from './msg-box/msg-box.component';
+import {
+    PaymentAuthorisationComponent
+} from './payment-authorisation/payment-authorisation.component';
+import { PaymentComponent } from './payment/payment.component';
 import { ProcessComponent } from './process/process.component';
-import { CanpayModalComponent } from './canpay-modal/canpay-modal.component';
-import { InputAmountComponent } from './input-amount/input-amount.component';
 import { CanPayService } from './services/canpay.service';
-import { ModuleWithProviders } from '@angular/compiler/src/core';
 import { CanYaCoinEthService } from './services/canyacoin-eth.service';
+import { EthService } from './services/eth.service';
 
 const COMPONENTS = [
   CanpayModalComponent,
@@ -54,7 +56,6 @@ const PROVIDERS = [EthService, CanYaCoinEthService, CanPayService];
 })
 export class CanpayModule {
   static forRoot(config: any): ModuleWithProviders {
-    console.log('canPayConfigs: ', config);
     return {
       ngModule: CanpayModule,
       providers: [{ provide: 'Config', useValue: config }]

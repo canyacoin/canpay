69369740d9e5cefa90e0eec204d2aab0df32c162
alsco77 <alsco77@pm.me>
2018-10-31 14:19:09 +0800

    Updated readme and example

--- projects/canpay-lib/src/lib/services/eth.service.ts
+++ projects/canpay-lib/src/lib/services/eth.service.ts
@@ -190,6 +190,8 @@ export class EthService implements OnDestroy {
           } else {
             resolve('11000000000');
           }
+        }).catch(e => {
+          resolve('11000000000');
         });
       } catch (e) {
         resolve('11000000000');

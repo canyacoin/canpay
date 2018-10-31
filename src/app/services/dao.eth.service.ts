a3304f2a2605aa5c7ef7d6278fc9a82a3b834267
alsco77 <alsco77@pm.me>
2018-10-31 13:54:37 +0800

    Made necessary changes to account for metamask upgrade

--- src/app/services/dao.eth.service.ts
+++ src/app/services/dao.eth.service.ts
@@ -17,17 +17,17 @@ export class DaoEthService extends EthService {
     this.daoContract = this.createContractInstance(daoAbi, environment.contracts.canYaDao);
   }
 
-  async createUserEscrow(fromAccount = this.account.value, amount, onTxHash: Function): Promise<any> {
+  async createUserEscrow(fromAccount = this.getOwnerAccount(), amount, onTxHash: Function): Promise<any> {
     console.log('createUserEscrow: ', this.daoContract, fromAccount, amount);
     return new Promise((resolve, reject) => {
       this.daoContract.methods.createUserEscrow(this.amountToCANTokens(amount))
         .send({ from: fromAccount, ...this.getDefaultGasParams() },
-          async (err, txHash) => this.eth.resolveTransaction(err, fromAccount, txHash, resolve, reject, onTxHash));
+          async (err, txHash) => this.resolveTransaction(err, fromAccount, txHash, resolve, reject, onTxHash));
       // send the onTxHash method here to utilise the callback
     });
   }
 
-  balanceOfUser(user = this.account.value) {
+  balanceOfUser(user = this.getOwnerAccount()) {
     return this.daoContract.methods.balanceOfUser(user).call();
   }
 
